22c4027703693fb7056c896d78daebe5889bad2e
Serhii Polishevskyi <s.polishevskiy@gmail.com>
2025-12-22 19:34:47 +0200

    Remove 'Mobile' from Telegram notification title

--- send-telegram-notification.sh
+++ send-telegram-notification.sh
@@ -44,7 +44,7 @@ else
 fi
 
 # Build message for Telegram
-MESSAGE="🚀 <b>Mobile Test Automation Completed!</b>
+MESSAGE="🚀 <b>Test Automation Completed!</b>
 
 📊 <b>Test Statistics:</b>
 • <b>Total tests:</b> $TOTAL_TESTS
